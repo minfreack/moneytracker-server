23ccde2158b5773e67cc888d0e9d3526ecd19164
luis <neto2racas@gmail.com>
2024-04-15 12:50:39 -0600

    Vercel config

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 const dotenv = require('dotenv')
 const express =  require('express');
 const cors = require('cors');
-const http = require('http');
-import { Server } from 'ws';
+// const http = require('http');
+const {WebSocketServer} = require('ws')
 const routes = require('./routes');
 const { getLastTransactions, newTransaction, getTransactions } = require('./components/cashflow/controller');
 
 const port = process.env.PORT || 8080;
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+// const server = http.createServer(app);
+// const io = new Server(server);
 dotenv.config()
 
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-  const wss = new Server({ port: 8080 });
+  const wss = new WebSocketServer({port: 8000});
 
   wss.on('connection', async(socket) => {
     const id = socket.handshake.query['id']
@@ -48,7 +48,7 @@ app.use(express.urlencoded({ extended: true }));
 
 routes(app)
 
-server.listen(port, () => {
+app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
 
