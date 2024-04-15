92bef6d4f3dd79d0aabadefcc9117a1a2fd33865
luis <neto2racas@gmail.com>
2024-04-15 12:42:16 -0600

    Vercel config

--- api/index.js
+++ api/index.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-export default (req, res) => {
   const wss = new Server({ noServer: true });
 
   wss.on('connection', async(socket) => {
@@ -46,7 +45,6 @@ export default (req, res) => {
         wss.to(id)?.emit('all-transactions', {data: res?.data?.data})
       }})
   });
-};
 
 routes(app)
 
