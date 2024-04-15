601bd886bb0f74f01126d46fc5ad2a0d3ba841d6
luis <neto2racas@gmail.com>
2024-04-15 12:40:11 -0600

    Vercel config

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv')
 const express =  require('express');
 const cors = require('cors');
 const http = require('http');
-const {Server} = require('socket.io')
+import { Server } from 'ws';
 const routes = require('./routes');
 const { getLastTransactions, newTransaction, getTransactions } = require('./components/cashflow/controller');
 
@@ -16,35 +16,37 @@ app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-io.on('connection', async(socket) => {
-  const id = socket.handshake.query['id']
-  socket.join(id)
-  const res = await getLastTransactions(id)
-  if(res?.data?.success){
-    io.to(id)?.emit('cashflow', {data: res?.data?.data})
-  }
-  socket.on('new-transaction', async(data) => {
-    const res = await newTransaction(data)
+export default (req, res) => {
+  const wss = new Server({ noServer: true });
+
+  wss.on('connection', async(socket) => {
+    const id = socket.handshake.query['id']
+    socket.join(id)
+    const res = await getLastTransactions(id)
     if(res?.data?.success){
-      const res = await getLastTransactions(data?.userID)
-      if(res?.data?.success){
-        io.to(id)?.emit('cashflow', {data: res?.data?.data})
-      }
-      const res2 = await getTransactions(data?.userID)
-      if(res2?.data?.success){
-        io.to(id)?.emit('all-transactions', {data: res2?.data?.data})
-      }
+      wss.to(id)?.emit('cashflow', {data: res?.data?.data})
     }
-  })
+    socket.on('new-transaction', async(data) => {
+      const res = await newTransaction(data)
+      if(res?.data?.success){
+        const res = await getLastTransactions(data?.userID)
+        if(res?.data?.success){
+          wss.to(id)?.emit('cashflow', {data: res?.data?.data})
+        }
+        const res2 = await getTransactions(data?.userID)
+        if(res2?.data?.success){
+          wss.to(id)?.emit('all-transactions', {data: res2?.data?.data})
+        }
+      }
+    })
 
-  socket.on('all-transactions', async(data) => {
-    const res = await getTransactions(data?.userID)
-    if(res?.data?.success){
-      io.to(id)?.emit('all-transactions', {data: res?.data?.data})
-    }})
+    socket.on('all-transactions', async(data) => {
+      const res = await getTransactions(data?.userID)
+      if(res?.data?.success){
+        wss.to(id)?.emit('all-transactions', {data: res?.data?.data})
+      }})
   });
-
-  
+};
 
 routes(app)
 
