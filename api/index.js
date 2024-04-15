d5ec86f7d81a8835bafdab5f050893bc583e310b
luis <neto2racas@gmail.com>
2024-04-15 12:44:33 -0600

    Vercel config

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
-  const wss = new Server({ noServer: true });
+  const wss = new Server({ port: 8080 });
 
   wss.on('connection', async(socket) => {
     const id = socket.handshake.query['id']
